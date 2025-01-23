d55dc42ea9a4f895be4e0cbb5cbac0c17cee343e
TechBhanwar <bhvrr2002@gamil.com>
2025-01-23 16:01:52 +0530

    Added section

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import Home from './pages/Home';
 import Shop from './pages/Shop';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Product from './pages/Product';
 import Profile from './auth/Profile';
 import Singup from './auth/Singup';
 import Login from './auth/Login';
@@ -15,7 +14,6 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/shop" element={<Shop />} />
-        <Route path="/product" element={<Product />} />
         <Route path="/profile" element={<Profile />} />
         <Route path="/sing_up" element={<Singup />} />
         <Route path="/login" element={<Login />} />
